3a877f244e93e68b01ead83bc1dd3662409d3a96
swapnaC <swapnachalla613@gmail.com>
2015-10-13 22:55:58 -0700

    all changes for buddies list

--- app/app.js
+++ app/app.js
@@ -1,12 +1,58 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+angular.module('buddyListApp', ['ngDialog'])
+      .service('buddyService', ['$http', '$q' ,function(http, $q) {
+        var deferred = $q.defer();
+        http.get('/app/buddiesData.json').then(function(data) {
+              deferred.resolve(data);
+        });
+
+        this.getBuddies = function() {
+            return deferred.promise;
+        }
+      }])
+      .controller('buddyListCntrl', ['$scope', 'ngDialog', 'buddyService', function (scope, ngDialog, buddyService) {
+          var promise = buddyService.getBuddies();
+          promise.then(function(data) {
+          scope.buddiesData = data.data;
+          });
+
+          scope.sortReverse = true;
+          scope.sortOrder = function(sortType) {
+            scope.key = sortType ? sortType : "userName";
+            scope.sortReverse = !scope.sortReverse;
+          }
+          scope.showDetailsView = function(idx) {
+            scope.currentIdx = idx;
+          }
+
+          scope.removeBuddy = function(entry) {
+            ngDialog.openConfirm({
+              template: "<h4>Do you want to delete " + entry.userName + "?</h4>\
+                <button type='button' class='ngdialog-button ngdialog-button-secondary' ng-click='closeThisDialog(0)'>No</button>\
+                <button type='button' class='ngdialog-button ngdialog-button-primary' ng-click='confirm()'>Yes</button>",
+              plain: true
+            }).then(function() {
+              //Confirm Promise
+              var idx = scope.buddiesData.indexOf(entry);
+              scope.buddiesData.splice(idx, 1);
+            });
+          }
+
+          scope.openAddDialog = function() {
+            ngDialog.open({
+              template: "addBuddyTemplate.html",
+              controller: "addBuddyCntrl",
+              scope: scope
+            });
+          }
+}])
+      .controller('addBuddyCntrl', ['$scope', '$filter', function(scope, $filter) {
+          scope.addBuddy = function() {
+            //add a default status to new buddy here
+            scope.user.status = "Offline";
+            scope.$parent.buddiesData.push(scope.user);
+            scope.closeThisDialog();
+          }
 }]);
+
